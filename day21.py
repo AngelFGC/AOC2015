76a427de29c2e7551fe5480604b0d328f52e0f54
Angel FGC <44098660+AngelFGC@users.noreply.github.com>
2023-01-19 17:00:44 +0900

    day 21 complete

--- day21.py
+++ day21.py
@@ -40,6 +40,7 @@ weapons = [
 ]
 
 armors = [
+    (0,0,0),
     (13,0,1),
     (31,0,2),
     (53,0,3),
@@ -48,6 +49,8 @@ armors = [
 ]
 
 rings = [
+    (0,0,0),
+    (0,0,0),
     (25,1,0),
     (50,2,0),
     (100,3,0),
@@ -58,14 +61,64 @@ rings = [
 
 def read():    
     with open("inputs/day21.txt", mode="+r", encoding="utf-8") as f:
-        return tuple(int(c) for c in f.read().strip())
+        return tuple(int(c) for c in f.read().strip().split(","))
+
+def getTurnsToWin(pHP, pAtk, pDef, boss):
+    bHP, bAtk, bDef = boss
+    return (
+        math.ceil(bHP/max(1, pAtk - bDef)),
+        math.ceil(pHP/max(1, bAtk - pDef))
+    )
 
 def part1(stdscrn, vis=False):
-    playerhp = 8
+    playerhp = 100
     boss_stats =  read()
+
+    mincost = float("+inf")
+    mingear = None
+
+    for (i, wpn), (j, arm), ((k1, hand1), (k2, hand2)) in itertools.product(
+        enumerate(weapons), enumerate(armors), 
+        itertools.combinations(enumerate(rings), 2)
+    ):
+        cost = wpn[0] + arm[0] + hand1[0] + hand2[0]
+        t2Win, t2Lose = getTurnsToWin(
+            playerhp, wpn[1] + arm[1] + hand1[1] + hand2[1],
+            wpn[2] + arm[2] + hand1[2] + hand2[2], boss_stats
+        )
+
+        if t2Win <= t2Lose:
+            if cost < mincost:
+                mincost = cost
+                mingear = (i,j-1, k1-2, k2-2)
+
+    print(mincost)
+    print(mingear)
     
 def part2(stdscrn, vis=False):
-    inpt = read()
+    playerhp = 100
+    boss_stats =  read()
+
+    maxcost = float("-inf")
+    maxgear = None
+
+    for (i, wpn), (j, arm), ((k1, hand1), (k2, hand2)) in itertools.product(
+        enumerate(weapons), enumerate(armors), 
+        itertools.combinations(enumerate(rings), 2)
+    ):
+        cost = wpn[0] + arm[0] + hand1[0] + hand2[0]
+        t2Win, t2Lose = getTurnsToWin(
+            playerhp, wpn[1] + arm[1] + hand1[1] + hand2[1],
+            wpn[2] + arm[2] + hand1[2] + hand2[2], boss_stats
+        )
+
+        if t2Win > t2Lose:
+            if cost > maxcost:
+                maxcost = cost
+                maxgear = (i,j-1, k1-2, k2-2)
+
+    print(maxcost)
+    print(maxgear)
 
 if __name__ == "__main__":
     # CURSES
@@ -76,11 +129,4 @@ if __name__ == "__main__":
         curses.start_color()
     
     part1(stdscr, vis=True)
-    #part2(stdscr, vis=True)
-
-
-"""
-Hit Points: 103
-Damage: 9
-Armor: 2
-"""
+    part2(stdscr, vis=True)
